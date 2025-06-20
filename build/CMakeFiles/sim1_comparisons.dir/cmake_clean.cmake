0e0e07acab9ec0537a85e6f97100984cde560b8f
Your Name <you@example.com>
2025-06-20 14:41:56 -0700

    added executables

---
+++ build/CMakeFiles/sim1_comparisons.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim1_comparisons.dir/src/simulate_baseline_comparisons.cpp.o"
+  "CMakeFiles/sim1_comparisons.dir/src/simulate_baseline_comparisons.cpp.o.d"
+  "bin/sim1_comparisons"
+  "bin/sim1_comparisons.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim1_comparisons.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sim2_slack_experiment.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sim2_slack_experiment.
+# This may be replaced when dependencies are built.
